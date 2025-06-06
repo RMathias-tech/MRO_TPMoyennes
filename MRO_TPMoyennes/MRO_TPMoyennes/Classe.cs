4f975a5478e3f45d2a544e45c731ee7b13f557d5
RMathias-tech <marobert@hn-services.fr>
2025-06-06 11:06:30 +0200

    Add files via upload

---
+++ MRO_TPMoyennes/MRO_TPMoyennes/Classe.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HNI_TPmoyennes
+{
+    internal class Classe
+    {
+        public string nomClasse { get; set; }
+        public List<Eleve> eleves { get; set; }
+        public List<string> matieres { get; set; }
+        public Classe(string nomClasse)
+        {
+            this.nomClasse = nomClasse;
+            this.eleves = new List<Eleve>();
+            this.matieres = new List<string>();
+        }
+        public void ajouterEleve(string prenom, string nom)
+        {
+            if (eleves.Count >= 30)
+            {
+                Console.WriteLine("La classe est pleine, impossible d'ajouter un nouvel élève.");
+                return;
+            }
+            eleves.Add(new Eleve(prenom, nom));
+        }
+        public void ajouterMatiere(string matiere)
+        {
+            if (matieres.Count >= 10)
+            {
+                Console.WriteLine("impossible d'ajouter une nouvelle matière.");
+                return;
+            }
+            matieres.Add(matiere);
+        }
+        public float moyenneMatiere(int matiereIndex)
+        {
+            return eleves.Average(e => e.moyenneMatiere(matiereIndex));
+        }
+        public float moyenneGeneral()
+        {
+            if (matieres.Count == 0)
+                return 0;
+            return Enumerable.Range(0, matieres.Count).Average(i => moyenneMatiere(i));
+        }
+        public static float Troncature2(float value)
+        {
+            return (float)(Math.Truncate(value * 100) / 100);
+        }
+    }
+}
---
+++ MRO_TPMoyennes/MRO_TPMoyennes/Eleve.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HNI_TPmoyennes
+{
+    internal class Eleve
+    {
+        public string prenom { get; set; }
+        public string nom { get; set; }
+        public List<Note> notes { get; set; }
+        public Eleve(string prenom, string nom)
+        {
+            this.prenom = prenom;
+            this.nom = nom;
+            this.notes = new List<Note>();
+        }
+        public void ajouterNote(Note note)
+        {
+            if (notes.Count < 200)
+            {
+                notes.Add(note);
+            }
+        }
+        public float moyenneMatiere(int matiereIndex)
+        {
+            var matiereNotes = notes.Where(n => n.matiere == matiereIndex).Select(n => n.note);
+            return matiereNotes.Any() ? matiereNotes.Average() : 0;
+        }
+        public float moyenneGeneral()
+        {
+            var matieres = notes.Select(n => n.matiere).Distinct();
+            if (!matieres.Any())
+                return 0;
+            return matieres.Average(m => moyenneMatiere(m));
+        }
+    }
+}
